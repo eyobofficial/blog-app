306bd9c986d91ebec6d1b7781a816f1395bb411e
eyob tariku <eyobtariku@gmail.com>
2018-03-16 23:39:38 +0300

    Create a Post model for blog posts

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    Abstracts a blog post
+    """
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique_for_date='publish')
+    author = models.ForeignKey(User, related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='draft',
+    )
+
+    class Meta:
+        ordering = ['-publish', ]
+
+    def get_absolute_url(self, *args, **kwargs):
+        return reverse('blogs:post-detail', args=[str(self.pk)])
+
+    def __str__(self):
+        return self.title
