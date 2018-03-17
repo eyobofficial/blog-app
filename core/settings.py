3e46ef1cacb741aae30fb31f3cdead2700549fe4
eyobtariku <eyobtariku@gmail.com>
2018-03-17 14:42:20 -0400

    Update allowed hosts to include codeanywhere

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9hxcp$ik=ehv@s8@hl=hw=my!s=+v4*hsjiwcqqswgdc8+5v+b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'blog-app-eyobtariku213805.codeanyapp.com', ]
 
 
 # Application definition
